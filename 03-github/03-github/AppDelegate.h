6103177274243ed0534c3885401003d28882d7a7
liang <280345503@qq.com>
2015-12-12 15:08:11 +0800

    初始化

---
+++ 03-github/03-github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  03-github
+//
+//  Created by 亮 on 15/12/12.
+//  Copyright © 2015年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 03-github/03-github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  03-github
+//
+//  Created by 亮 on 15/12/12.
+//  Copyright © 2015年 liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
